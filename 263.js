ef37a4db5e8560b710ff7e489c7edc30fe33ee53
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-12-07 13:16:16 +0500

    added two problems

---
+++ 263.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+  while (n > 0) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else if (n % 3 == 0) {
+      n = n / 3;
+    } else if (n % 4 == 0) {
+      n = n / 5;
+    } else {
+      return false;
+    }
+  }
+  if (n == 1) {
+    return true;
+  }
+};
